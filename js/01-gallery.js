ac2b6a4886bdd989157efce50dfc5827459bea2e
SergiiS95 <SSirota1995@gmail.com>
2023-05-02 10:21:08 +0300

    v1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryImg = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryImg);
+
+gallery.addEventListener("click", onClickImege);
+
+function onClickImege(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  function onCloseModal(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      gallery.removeEventListener("keydown", onCloseModal);
+    }
+  }
+
+  gallery.addEventListener("keydown", onCloseModal);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+const galleryImg = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryImg);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
